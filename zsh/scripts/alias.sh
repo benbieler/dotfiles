6405864c5576d176bf7a75348a8aea36f386a1f6
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-12-25 22:48:14 +0100

    improve ZSH configuration and add more completion/alias plugins to the setup

---
+++ zsh/scripts/alias.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# basic dir shortcuts
+alias ~="cd ~"
+alias proj="cd ~/Projects"
+
+# missing interpreter shortcuts for ruby/php/nodejs
+alias rb="ruby"
+alias cls="clear screen"
+
+alias phi="php -a"
+alias p="php"
+
+alias ni="npm install"
+alias nis="npm install --save"
+alias nid="npm install --save-dev"
+alias nig="npm install -g"
+
+# directory movement commands
+alias mkdirp="mkdir -p"
+alias l="ls -la"
+
+# vagrant shortcuts
+alias vup="vagrant up"
+alias vssh="vagrant ssh"
+alias vrl="vagrant reload"
+alias vr="vagrant resume"
+alias vs="vagrant suspend"
+alias vdstr="vagrant destroy"
+
+# shortcuts for some projects and dirs
+alias dotf="cd ~/Projects/dotfiles"
+alias apik="cd ~/Projects/Ma27ApiKeyAuthenticationBundle"
+alias sent="cd ~/Projects/Sententiaregum"
+alias eslv="cd ~/Projects/eslint-plugin-varspacing"
+alias dtvm="cd ~/Projects/deployment-test-vm"
+alias ppmd="cd ~/Projects/puppet-module-devel"
+alias ppnd="cd ~/Projects/puppet-module-devel/dev/nodejs"
+alias ppcp="cd ~/Projects/puppet-module-devel/dev/composer"
+
+alias docs="cd ~/Documents"
+alias pics="cd ~/Pictures"
+alias dwnl="cd ~/Downloads"
+
+# credits to http://askubuntu.com/questions/365965/how-to-remove-all-rc-residual-packages-using-command-line-at-once
+alias remove_rc_pkgs="sudo apt-get remove --purge $(dpkg -l | grep "^rc" | awk '{print $2}' | tr '\n' ' ')"
